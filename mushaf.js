15c109bdcbd3ebc56d7c52e025d6b3d476ef66e5
saqfish <saqfish@gmail.com>
2021-05-29 02:49:07 -0500

    init

---
+++ mushaf.js
@@ -0,0 +1,31 @@
+const mushafs = require("./mushafs");
+
+const mushaf = (rawi) => {
+  let chapters, verses, raw, parts, sections;
+
+  try {
+    chapters = require(`./data/${rawi}/chapters.json`);
+    verses = require(`./data/${rawi}/verses.json`);
+    raw = require(`./data/${rawi}/raw.json`);
+    parts = require(`./data/${rawi}/sections.json`);
+    sections = [];
+    if (parts) {
+      for (let p of parts) {
+        sections = sections.concat(p);
+      }
+    }
+  } catch (e) {
+    chapters = verses = raw = sections = [];
+  }
+
+  return {
+    // Data
+    chapters,
+    verses,
+    raw,
+    parts,
+    sections,
+  };
+};
+
+module.exports = mushaf(null);
